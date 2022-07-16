5860b580d1fc0302fe666a744af69cc44de4a2b6
OnahGodwin <88001517+GodwinOnah@users.noreply.github.com>
2022-07-16 14:11:13 +0100

    Added the first image using the 'Tilt' property

---
+++ Documents/GitHub/obandecars/Obance_Face_Police/obande_face_police/src/Logo.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+
+import Tilt from 'react-tilt';
+
+import image1 from './images'
+
+
+
+
+
+const Logo=()=>{return(
+
+	<div>
+
+<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 500, width: 250 }} >
+ <div className="Tilt-inner" style={{ padding: 100}}> <img style={{ paddingTop: 100, height:300, width:200}}src={image1} alt="..."/></div>
+</Tilt>
+	</div>
+
+
+
+)}
+
+
+export default Logo;
---
+++ Documents/GitHub/obandecars/Obance_Face_Police/obande_face_police/src/images.js
@@ -0,0 +1,3 @@
+import image1 from './images/image1.jpeg'
+
+export default image1;
